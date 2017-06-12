7ea6c96f722258e10df50b3a43529925f200a59a
Анисько Дмитрий Геннадьевич <vorobushek221@gmail.com>
2017-06-13 00:07:18 +0300

    Status bar and android navigation bar are hidden while app is running

--- ChessClock/ChessClock.Android/MainActivity.cs
+++ ChessClock/ChessClock.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace ChessClock.Droid
 {
@@ -17,10 +19,39 @@ namespace ChessClock.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+            this.Window.DecorView.SystemUiVisibility = StatusBarVisibility.Hidden;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                var statusBarHeightInfo = typeof(FormsAppCompatActivity)
+                    .GetField("_statusBarHeight", 
+                        System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                statusBarHeightInfo.SetValue(this, 0);
+            }
+
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+
+            var decorView = Window.DecorView;
+            StatusBarVisibility uiOptions = decorView.SystemUiVisibility;
+            var newUiOptions = (int)uiOptions;
+
+            newUiOptions |= (int)SystemUiFlags.LowProfile;
+            newUiOptions |= (int)SystemUiFlags.Fullscreen;
+            newUiOptions |= (int)SystemUiFlags.HideNavigation;
+            newUiOptions |= (int)SystemUiFlags.Immersive;
+            newUiOptions |= (int)SystemUiFlags.ImmersiveSticky;
+            newUiOptions |= (int)SystemUiFlags.LayoutFullscreen;
+
+            decorView.SystemUiVisibility = (StatusBarVisibility)newUiOptions;
+
+            this.Window.AddFlags(WindowManagerFlags.KeepScreenOn);
+            Forms.SetTitleBarVisibility(AndroidTitleBarVisibility.Never);
         }
     }
 }
